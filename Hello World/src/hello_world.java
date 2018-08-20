0b59fe4331a0ad1c0300f7e5a9a856e446f545ce
Winnie Liang <19liangy49@stu.smuhsd.org>
2018-08-20 16:37:40 -0700

    8/20/18 4:37PM

--- Hello World/src/hello_world.java
+++ Hello World/src/hello_world.java
@@ -9,6 +9,6 @@ public class hello_world
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello World! From "Winnie"); //Display the string"
+    	System.out.println("Hello World! From Winnie"); //Display the stringing"
     }
 }
