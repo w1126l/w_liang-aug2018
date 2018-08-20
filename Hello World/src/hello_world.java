4f1a74f93ab328be2469c200ac95cc1a293ad6c5
Winnie Liang <19liangy49@stu.smuhsd.org>
2018-08-20 09:51:59 -0700

    First Commit
    :3

--- Hello World/src/hello_world.java
+++ Hello World/src/hello_world.java
@@ -1,9 +1,14 @@
 
-public class hello_world {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+/**
+ * First Run
+ * 
+ * @author Winnie Liang
+ * @version 8/20/18
+ */
+public class hello_world
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From "Winnie"); //Display the string"
+    }
 }
